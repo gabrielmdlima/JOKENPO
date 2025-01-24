5ea05d3d93d98bd26b63b1e311afee010fbff509
Gabriel Lima <gabriel.mdlima@gmail.com>
2025-01-24 11:05:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from random import randint
 import utils
 
 
-class GlobalVariables:
+class GameData:
   def __init__(self):
     self.num_partidas = 0
     self.score_player = 0
@@ -16,14 +16,14 @@ def computer_choice():
   return choice
 
 
-def count_score(self, result):
+def count_score(data, result):
   if result == 1:
-    self.score_player += 1
+    data.score_player += 1
   elif result == 2:
-    self.score_computer += 1
+    data.score_computer += 1
 
 
-def run(self):
+def run(data):
 
   while True:    
     computer = computer_choice()
@@ -36,17 +36,17 @@ def run(self):
 
     result = utils.define_results(player, computer)  # Compara as escolhas do jogador e do computador, indicando se houve empate ou quem ganhou.
 
-    count_score(self, result)  # Contabiliza o placar.
+    count_score(data, result)  # Contabiliza o placar.
 
-    self.num_partidas += 1  # Contabiliza a quantidade de partidas.
+    data.num_partidas += 1  # Contabiliza a quantidade de partidas.
 
     print(utils.result(result))  # Apresenta o resultado no console.
 
     if not utils.continuar():  # Recebe a escolha do usuário de continuar ou não.
       break
 
-  utils.placar(self.score_player, self.score_computer, self.num_partidas)  # Imprime o placar ao final do jogo.
+  utils.placar(data.score_player, data.score_computer, data.num_partidas)  # Imprime o placar ao final do jogo.
 
 if __name__ == '__main__':
-  self = GlobalVariables()
-  run(self)
+  data = GameData()
+  run(data)
